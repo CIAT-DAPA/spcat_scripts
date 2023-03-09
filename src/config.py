95e624d66214016be5a27c4bea020b51c78ce034
Victor Hernandez <v.hernandez@cgiar.org>
2023-03-09 07:29:46 -0500

    GAP-34 added error handling

--- src/config.py
+++ src/config.py
@@ -7,8 +7,13 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 path_inputs = os.path.abspath(os.path.join(dir_path, 'Inputs'))
 path_outputs = os.path.abspath(os.path.join(dir_path, 'Outputs'))
 path_database = os.path.abspath(os.path.join(dir_path, 'database.xlsx'))
-database = pd.read_excel(path_database)
 
+try:
+    database = pd.read_excel(path_database)
+except Exception as e:
+    print('Problem reading database.xlsx file')
+    print(e)
+    sys.exit(1)
 
 cols_country = ['iso_2', 'name']
 cols_crop = ['ext_id', 'name', 'base_name', 'app_name']
@@ -21,10 +26,19 @@ def get_parameter(name):
     return str(value)
 
 def connect_db():
-    host = get_parameter('host')
-    port = get_parameter('port')
-    user = get_parameter('user')
-    password = get_parameter('password')
-    name_db = get_parameter('name_db')
-    db_url = "mongodb://" + user + ":" + password + "@" + host + ":" + port
-    return connect(host=db_url, db=name_db)
+    try:
+        host = get_parameter('host')
+        port = get_parameter('port')
+        user = get_parameter('user')
+        password = get_parameter('password')
+        name_db = get_parameter('name_db')
+        db_url = "mongodb://" + user + ":" + password + "@" + host + ":" + port
+        connection = connect(host=db_url, db=name_db)
+        connection.server_info()
+        print("Connection to database successful!")
+        return connection
+    except Exception as e:        
+        print("Error connecting to MongoDB:")
+        print(e)
+        print("Please check the parameters in the file database.xlsx")
+        sys.exit(1)
